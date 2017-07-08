b754cf24c81c2c0de78fe157bb708cd917b09f62
Holger Peters <email@holger-peters.de>
2017-07-08 20:03:52 +0200

    Initial commit

---
+++ components/PeopleListing.vue
@@ -0,0 +1,17 @@
+  <template>
+    <ul>
+        <li v-for="person in people" v-bind:key="person.guid">
+            {{ person.name }}
+            <router-link :to="{ name: 'person', params: { id: person.guid }}">View Details</router-link>
+        </li>
+    </ul>
+</template>
+
+
+  <script>
+// Listing people component
+export default {
+    name: 'PeopleListing',
+    props: ['people']
+};
+</script>
